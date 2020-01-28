863e9e68dc420cda6dee76cad2f7d1a193ebdbaa
Ayushgupta18 <itsayushgupta18@gmail.com>
2020-01-28 19:17:33 +0530

    making it better

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import {history} from './components/history'
 
-ReactDOM.render(<Router history={history}><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<Router history={history} basename='https://git.heroku.com/clarify-frontend.git'><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
